a01a969f867ca728a67e8d1de6ba8c092411a9f2
ethanarndt7 <111255027+ethanarndt7@users.noreply.github.com>
2023-07-06 16:26:12 -0400

    Update app.js

--- Kanye Quotes/app.js
+++ Kanye Quotes/app.js
@@ -3,7 +3,7 @@ function fetchQuote() {
       .then(response => response.json())
       .then(data => {
         const quoteContainer = document.getElementById('quote-container');
-        quoteContainer.innerText = data.quote + ' - Kanye West';
+        quoteContainer.innerText = data.quote + '\n - Kanye West';
       })
       .catch(error => {
         console.error(error);
